61a9d637bea95acfa177c664533eed75aad6cc1c
C:\Users\setor\Desktop\GitHub <seto.ryudou@gmail.com>
2025-08-16 21:17:05 +0900

    ゴール作成、横移動

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody PlayerRb;
-    const int SPEED = 500;
+    const int SPEED = 600;
+    const int POWER = 400;
 
-
+    bool finish;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +18,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (finish) {return;}
+
+        //ëOÇ…êiÇﬁèàóù
         PlayerRb.AddForce(SPEED * transform.forward * Time.deltaTime);
+        //â°Ç…à⁄ìÆ
+        //ç∂
+        if (Input.GetKey(KeyCode.A))
+        {
+            PlayerRb.AddForce(POWER * -transform.right * Time.deltaTime);
+        }
+        //âE
+        if (Input.GetKey(KeyCode.D))
+        {
+            PlayerRb.AddForce(POWER * transform.right * Time.deltaTime);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Finish")
+        {
+            finish = true;  
+            PlayerRb.linearVelocity = Vector3.zero;
+        }
     }
 }
